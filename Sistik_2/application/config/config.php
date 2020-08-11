df14a16d8736c5801e8b4e3c44e548c101dee63f
Ikhsandi <60900117039@uin-alauddin.ac.id>
2020-08-11 10:24:30 +0800

    Sandi

--- Sistik_2/application/config/config.php
+++ Sistik_2/application/config/config.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /*
 |--------------------------------------------------------------------------
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/sistik/Statistik_2';
+$config['base_url'] = 'http://localhost/SI_Statistik/Sistik_2/';
 
 /*
 |--------------------------------------------------------------------------
